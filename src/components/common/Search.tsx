61c189ccc33003cce394efc8becb1c9811666a23
Mahmoud Mohsen <mahmoud.mohsen.developer@gmail.com>
2024-12-04 07:35:22 +0200

    fixed eslint error

--- src/components/common/Search.tsx
+++ src/components/common/Search.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useMyContext } from '@/context/Store';
-import { InputProps } from 'antd';
 import { MeiliSearch } from 'meilisearch';
 import React from 'react';
 import { LuSearch } from 'react-icons/lu';
@@ -14,53 +13,48 @@ const client = new MeiliSearch({
 
 export type SearchProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-const Search = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, ...props }, ref) => {
-    const { setSearchData } = useMyContext();
-    const [searchTerm, setSearchTerm] = React.useState('');
+const Search = () => {
+  const { setSearchData } = useMyContext();
+  const [searchTerm, setSearchTerm] = React.useState('');
 
-    const onSearchSubmit = async (e: any) => {
-      e.preventDefault();
-      const results = await client.multiSearch({
-        queries: [
-          {
-            indexUid: 'product',
-            q: searchTerm,
-            limit: 10
-          },
-          {
-            indexUid: 'blog',
-            q: searchTerm,
-            limit: 5
-          }
-        ]
-      });
+  const onSearchSubmit = async (e: any) => {
+    e.preventDefault();
+    const results = await client.multiSearch({
+      queries: [
+        {
+          indexUid: 'product',
+          q: searchTerm,
+          limit: 10
+        },
+        {
+          indexUid: 'blog',
+          q: searchTerm,
+          limit: 5
+        }
+      ]
+    });
 
-      console.log('results', results);
-      if (results) {
-        setSearchData(results);
-      }
-      // TODO: Render results
-    };
+    console.log('results', results);
+    if (results) {
+      setSearchData(results);
+    }
+  };
 
-    return (
-      <form
-        className={`flex w-full grow items-center gap-3 ${className ?? ''}`}
-        onSubmit={onSearchSubmit}
-      >
-        <LuSearch className='text-gray-medium' size={24} />
-        <input
-          // {...props}
-          type='search'
-          placeholder='Search...'
-          ref={ref}
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className='w-[calc(100%-70px)] bg-transparent p-2 px-2 outline-none placeholder:text-gray-medium focus-visible:text-black-medium'
-        />
-      </form>
-    );
-  }
-);
+  return (
+    <form
+      className={`flex w-full grow items-center gap-3`}
+      onSubmit={onSearchSubmit}
+    >
+      <LuSearch className='text-gray-medium' size={24} />
+      <input
+        type='search'
+        placeholder='Search...'
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className='w-[calc(100%-70px)] bg-transparent p-2 px-2 outline-none placeholder:text-gray-medium focus-visible:text-black-medium'
+      />
+    </form>
+  );
+};
 
 export default Search;
