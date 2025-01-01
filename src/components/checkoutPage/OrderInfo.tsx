584d4e90793d279583580a2a2fe002f855b4e588
Mahmoud Mohsen <mahmoud.mohsen.developer@gmail.com>
2025-01-01 21:37:46 +0200

    [2] fixed deployment error

--- src/components/checkoutPage/OrderInfo.tsx
+++ src/components/checkoutPage/OrderInfo.tsx
@@ -9,8 +9,6 @@ import { useLocale, useTranslations } from 'next-intl';
 import ShippingCost from './ShippingCost';
 import { useForm } from 'antd/es/form/Form';
 import { ShippingCostDataType } from '@/types/shippingCostResponseTypes';
-// import { FreeShippingAttributesType } from '@/types/freeShippingResponseType';
-// import { useEffect, useState } from 'react';
 import { useMyContext } from '@/context/Store';
 import { ValidateErrorEntity } from 'rc-field-form/lib/interface';
 import { fetchGraphqlClient } from '@/services/graphqlCrud';
@@ -20,27 +18,16 @@ import {
   getIdFromToken
 } from '@/utils/cookieUtils';
 import { UpdateGuestUserResponseType } from '@/types/guestUserReponses';
-// import {
-//   CreateAddressResponseType,
-//   updateAddressResponseType
-// } from '@/types/addressResponseTypes';
 import { CreateOrderResponseType } from '@/types/orderResponseTypes';
 import { CartDataType } from '@/types/cartResponseTypes';
 import { uploadInvoicePdf } from '@/services/invoicesPDFHandlers';
 import { capitalize } from '@/utils/helpers';
 import { useRouter } from '@/navigation';
-import {
-  // CustomerPaymentDetailsType,
-  PaymentDataType
-  // PaymentRequest
-} from '@/types/paymentResonseType';
+import { PaymentDataType } from '@/types/paymentResonseType';
 import { fetchGraphqlServerWebAuthenticated } from '@/services/graphqlCrudServerOnly';
 import { createAddress } from '@/services/shippingAddress';
-import { useEffect, useState } from 'react';
 import { useUser } from '@/context/UserContext';
-import { AdressesType } from '@/types/addressResponseTypes';
-import { PiNutFill } from 'react-icons/pi';
-import { getDefaultActiveAddressId } from '@/app/[locale]/account/addresses/page';
+import { getDefaultActiveAddressId } from '@/services/handleAddresses';
 
 const updateGuestUserQuery = (
   guestUserId: string,
